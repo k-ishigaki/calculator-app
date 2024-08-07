5fbeb998dbcb4df6d48f203343930269375c4476
Kazuki Ishigaki <gakia5310027@gmail.com>
2024-08-07 23:31:56 +0900

    2024-08-07

--- shared/src/commonMain/kotlin/io/github/kishigaki/calculatorapp/domain/parser/BinaryOperations.kt
+++ shared/src/commonMain/kotlin/io/github/kishigaki/calculatorapp/domain/parser/BinaryOperations.kt
@@ -3,33 +3,25 @@ package io.github.kishigaki.calculatorapp.domain.parser
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.binding
 
 class BinaryOperations<T>(private val primary: Parser<T>, private vararg val constructionRules: Pair<Symbol, ((T, T) -> T)>) : Parser<T> {
-    override fun parse(text: String): Result<Pair<T, String>, Pair<Error, String>> {
-        val firstResult = primary.parse(text)
-        if (firstResult.isErr) {
-            return Err(firstResult.error)
-        }
+    override fun parse(text: String) = binding {
+        var parsed: Pair<T, String> = primary.parse(text).bind()
 
-        var parsed: Pair<T, String> = firstResult.value
-
-        var continues = true
-        while (continues) {
-            continues = false
-            constructionRules.forEach { rule ->
-                val operatorParsed = rule.first.parse(parsed.second)
-                if (operatorParsed.isErr) {
-                    return@forEach
+        do {
+            val consumed = constructionRules.map { rule ->
+                binding {
+                    val operatorParsed = rule.first.parse(parsed.second).bind()
+                    val secondOperandParsed = primary.parse(operatorParsed.second).bind()
+                    parsed = rule.second(
+                        parsed.first,
+                        secondOperandParsed.first
+                    ) to secondOperandParsed.second
                 }
-                val secondOperandParsed = primary.parse(operatorParsed.value.second)
-                if (secondOperandParsed.isErr) {
-                    return@forEach
-                }
-                continues = true
-                parsed = rule.second(parsed.first, secondOperandParsed.value.first) to secondOperandParsed.value.second
-            }
-        }
+            }.any { it.isOk }
+        } while (consumed)
 
-        return Ok(parsed)
+        parsed
     }
 }
